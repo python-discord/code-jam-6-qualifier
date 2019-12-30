7f0379a7349c596ee36317b4e9178f200e41f208
Sebastiaan Zeeff <33516116+SebastiaanZ@users.noreply.github.com>
2019-12-30 20:53:32 +0100

    Update qualifier.py

--- alternative-2-kivy-application/qualifier.py
+++ alternative-2-kivy-application/qualifier.py
@@ -22,16 +22,17 @@ from kivy.lang import Builder
 # will similarly declare a tree of widget children, properties and
 # canvas instructions, that will be automatically applied to all
 # instances of these classes, these rules are declarede using the class
-# name around pointy brackets, e.g: "<MyCustomLabel>:".would declare a
+# name around pointy brackets, e.g: "<MyCustomLabel>:" would declare a
 # rule applied to all instances of the MyCustomLabel class (which you
 # could define as a subclass of kivy.uix.label.Label).
 # The string can contain at most one "root" rule, which is a rule
 # without <> around its name, this rule is treated differently, it
-# directly instanciates a widget of the class, and adds the declared
+# directly instantiates a widget of the class, and adds the declared
 # properties, graphical instructions and children widgets to it. This
 # widget is returned by the Builder.load_string (or Builder.load_file)
 # call, so it can be added to the widget tree, either as root, or to an
-# existing parent widget of your choice.
+# existing parent widget of your choice. In the example below, "Label"
+# is the "root rule".
 
 KV_RULES = """
 Label:
